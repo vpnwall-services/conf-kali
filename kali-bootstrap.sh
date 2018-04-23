9b7e053d53c904a8f53fff2b9134a52b86a2207d
root <root@localhost.localdomain>
2018-04-23 15:51:16 +0000

    initial commit

---
+++ kali-bootstrap.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+
+MYPWD=$(pwd)
+XTRKPATH="/tmp/"
+tar xf $MYPWD/conf-kali.tar -C /tmp
+cp -r $XTRKPATH/ohmyzsh /root/.oh-my-zsh
+cp  $XTRKPATH/zshrc /root/.zshrc
+cp  $XTRKPATH/robby-theme /root/.oh-my-zsh/themes/robbyrussell.zsh-theme
+chsh -s /usr/bin/zsh
+cp  $XTRKPATH/tmux.conf /etc/
+zsh
