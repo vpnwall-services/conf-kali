f291a70bbe5bd978d126912b97f6008bf03b803c
Vpnwall Services <38083758+vpnwall-services@users.noreply.github.com>
2018-04-24 03:26:25 +0200

    Update kali-bootstrap.sh
    Wrong path for extraction

--- kali-bootstrap.sh
+++ kali-bootstrap.sh
@@ -2,7 +2,7 @@
 #git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 
 MYPWD=$(pwd)
-XTRKPATH="/tmp/"
+XTRKPATH="/tmp/conf-kali"
 tar xf $MYPWD/conf-kali.tar -C /tmp
 cp -r $XTRKPATH/ohmyzsh /root/.oh-my-zsh
 cp  $XTRKPATH/zshrc /root/.zshrc
